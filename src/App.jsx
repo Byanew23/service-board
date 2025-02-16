629f52efb636f14f8ccda63ed55460a19a354bbf
Byanew23 <borislav.yanev23@gmail.com>
2025-02-16 13:49:59 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Header, Footer, LandingPage } from './components';
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <LandingPage />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
