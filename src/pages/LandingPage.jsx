50c19ad96425250348da9026e6de914b28a74995
Byanew23 <borislav.yanev23@gmail.com>
2025-02-20 20:32:22 +0200

    add dotenv

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,3 +1,7 @@
+import axios from 'axios'
+
 export const LandingPage = () => {
+
+
     return <div>landing page</div>
 }
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
+import { config } from 'dotenv'
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 
+config()
+
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    define: {
+        "process.env": process.env
+    },
     server: {
+        cors: false,
         port: 3000,
     },
 });
